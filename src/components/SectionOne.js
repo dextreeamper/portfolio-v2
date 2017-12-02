fc56d1dc168ee6ec1bc1a0c39f9d579cf8fe236f
Dextree Amper <appsdex92@gmail.com>
2017-12-02 12:55:08 +0800

    Creating component for SectionOne and Two

---
+++ src/components/SectionOne.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import image from '../assets/img/painted-lady-1568926_960_720.jpg';
+
+class SectionOne extends Component {
+  render() {
+    return (
+      <section className="section-one">
+        <div className="main-panel">
+          <div className="title">
+            <h1>Dexter</h1>
+            <h1>Amper</h1>
+          </div>
+          <div className="blurb">
+            <p>
+              Dexter specializes in making things for human people. By day he crafts digital interfaces at Google. By night he educates others through video, audio, and the written word.
+            </p>
+          </div>
+          <div className="image">
+          </div>
+          <div className="section-number">
+            01
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default SectionOne;
---
+++ src/components/SectionTwo.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+class SectionTwo extends Component {
+  render() {
+    return (
+      <section className="section-two">
+        <div className="main-panel">
+          <div className="blurb">
+            <p>
+              Dexter specializes in making things for human people. By day he crafts digital interfaces at Google. By night he educates others through video, audio, and the written word.
+            </p>
+          </div>
+            <div className="column left--panel">
+              <div className="twoColumn">
+                <h3><strong>1.</strong> Visual Design</h3>
+              </div>
+              <div className="twoColumn">
+                <h3><strong>2.</strong> Visual Design</h3>
+              </div>
+              <div className="twoColumn">
+                <h3><strong>3.</strong> Visual Design</h3>
+              </div>
+              <div className="twoColumn">
+                <h3><strong>4.</strong> Visual Design</h3>
+              </div>
+              <div className="twoColumn">
+                <h3><strong>5.</strong> Visual Design</h3>
+              </div>
+            </div>
+            <div className="column right--panel">
+              <div className="twoColumn">
+                <h3><strong>6.</strong> Visual Design</h3>
+              </div>
+              <div className="twoColumn">
+                <h3><strong>7.</strong> Visual Design</h3>
+              </div>
+              <div className="twoColumn">
+                <h3><strong>8.</strong> Visual Design</h3>
+              </div>
+              <div className="twoColumn">
+                <h3><strong>9.</strong> Visual Design</h3>
+              </div>
+              <div className="twoColumn">
+                <h3><strong>10.</strong> Visual Design</h3>
+              </div>
+            </div>
+          <div className="section-number">
+            02
+          </div>
+          <div className="title">
+            <h1>My Skills</h1>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default SectionTwo;
