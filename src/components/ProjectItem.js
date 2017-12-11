3905d1ed6c6b8bd546b600bb5d322fcb7c93af35
Dextree Amper <appsdex92@gmail.com>
2017-12-11 18:17:12 +0800

    Adding projects and images to portfolio components

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,49 +1,60 @@
 import React, { Component } from 'react';
 import projectOne from '../assets/img/project-one.jpg';
-import projectTwo from '../assets/img/basin.jpg';
-import projectThree from '../assets/img/houston.jpg';
+import sam from '../assets/img/project-sam.png';
+import singlecell from '../assets/img/project-singlecell.png';
+import shapeyourmind from '../assets/img/project-shapeyourmind.png';
+import userprofile from '../assets/img/project-userprofile.png';
 import { Grid , Segment } from 'semantic-ui-react';
 
 class ProjectItem extends Component {
   render() {
     return (
       <div className="project-container">
-        <div className="item">
-          <div className="project--image even">
-            <img src={projectOne} />
+        <a href="https://github.com/dextreeamper/browser-software.git">
+          <div className="item">
+            <div className="project--image even">
+              <img src={sam} />
+            </div>
+            <div className="project--content even">
+              <p className="project--title"><strong>Sam Browse A Software</strong></p>
+              <p>A browsing software platform that allows you to search any software for your business.</p>
+            </div>
           </div>
-          <div className="project--content even">
-            <p className="project--title"><strong>Project A</strong></p>
-            <p>The quick brown fox jumps over the lazy dog at the bank of the river.</p>
+        </a>
+        <a href="http://singlecell.com.au/">
+          <div className="item">
+            <div className="project--image odd">
+              <img src={singlecell} />
+            </div>
+            <div className="project--content odd">
+              <p className="project--title"><strong>Singlecell IT Consulting</strong></p>
+              <p>An IT Consulting company with a service based platform that aligns with client’s process.</p>
+            </div>
           </div>
-        </div>
-        <div className="item">
-          <div className="project--image odd">
-            <img src={projectOne} />
+        </a>
+        <a href="http://shapeyourmind.com.au/">
+          <div className="item">
+            <div className="project--image even">
+              <img src={shapeyourmind} />
+            </div>
+            <div className="project--content even">
+              <p className="project--title"><strong>Shapeyourmind</strong></p>
+              <p>A simple viewing website for Dietitian with a booking consultation functionality.</p>
+            </div>
           </div>
-          <div className="project--content odd">
-            <p className="project--title"><strong>Project B</strong></p>
-            <p>The quick brown fox jumps over the lazy dog at the bank of the river.</p>
+        </a>
+        <a href="https://github.com/dextreeamper/user-profile-react.git">
+          <div className="item">
+            <div className="project--image odd">
+              <img src={userprofile} />
+            </div>
+            <div className="project--content odd">
+              <p className="project--title"><strong>Simple User Profile</strong></p>
+              <p>A simple user profile layout using React and Redux with github user profile design.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="item">
-          <div className="project--image even">
-            <img src={projectOne} />
-          </div>
-          <div className="project--content even">
-            <p className="project--title"><strong>Project C</strong></p>
-            <p>The quick brown fox jumps over the lazy dog at the bank of the river.</p>
-          </div>
-        </div>
-        <div className="item">
-          <div className="project--image odd">
-            <img src={projectOne} />
-          </div>
-          <div className="project--content odd">
-            <p className="project--title"><strong>Project D</strong></p>
-            <p>The quick brown fox jumps over the lazy dog at the bank of the river.</p>
-          </div>
-        </div>
+        </a>
       </div>
     );
   }
